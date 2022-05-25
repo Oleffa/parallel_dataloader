2d8f30c75cd58017b9b70ae3613dc5ae911dd7ae
Oliver <oliver.struckmeier@tutanota.de>
2022-05-25 15:54:39 +0300

    Working dataloader and more tests

--- src/dataloader.py
+++ src/dataloader.py
@@ -10,22 +10,6 @@ import time
 from queue import Queue
 import psutil
 
-class RAMLoader(threading.Thread):
-    def __init__(self, ds, ram, files, file_load_list, load_per_file):
-        pass
-    def load_files(self, index):
-        pass
-    def get_ram_size(self):
-        pass
-    def stop(self):
-        pass
-    def pause(self):
-        pass
-    def resume(self):
-        pass
-    def run(self):
-        pass
-
 class DataLoader():
     def __init__(self, dataset_params):
         self.dp = dataset_params
@@ -36,75 +20,197 @@ class DataLoader():
     def get_loader(self):
         return self.loader, self.dataset
 
+class MemoryLoader(threading.Thread):
+    def __init__(self, memory_size, memory, one_file_size, data_info, fits_memory, \
+            data_labels, shuffle, device, verbose):
+        threading.Thread.__init__(self)
+        self.data_labels = data_labels
+        self.data_info = data_info
+        self.memory_size = memory_size
+        self.one_file_size = one_file_size
+        self.memory = memory
+        self.running = True
+        self.fits_memory = fits_memory
+        self.shuffle = shuffle
+        self.device = device
+        self.v = verbose
+
+        self.load_list = np.arange(0, len(data_info[data_labels[0]]), 1)
+        if self.shuffle:
+            np.random.shuffle(self.load_list)
+        self.load_idx = 0
+
+        self.update_cur_memory_size()
+        
+    def update_cur_memory_size(self):
+        """Computes the total amount of data in memory in mb and updates
+        the local variable. Also checks for overflows.
+        """
+        self.cur_memory_size = 0.0
+        for i in list(self.memory.queue):
+            self.cur_memory_size += i['size']
+        assert self.cur_memory_size <= self.memory_size
+
+    def stop(self):
+        self.running = False
+
+    def load_file(self, dl, load_idx):
+        data_name_target = self.data_info[dl][load_idx]['data_name']
+        with h5py.File(self.data_info[dl][load_idx]['data_path']) as f:
+            for gname, group in f.items():
+                for data_label, group2 in group.items():
+                    for data_name, ds in group2.items():
+                        if data_label == dl and \
+                                data_name == data_name_target:
+                            d = np.array(ds)
+                            if self.shuffle:
+                                for i in d:
+                                    np.random.shuffle(i)
+                            d = torch.from_numpy(d).to(self.device)
+                            return d, self.data_info[dl][load_idx]['fsize']
+
+    def run(self):
+        while self.running:
+            # If one more file fits in the RAM
+            self.update_cur_memory_size()
+            while self.cur_memory_size + self.one_file_size <= self.memory_size:
+                if not self.running:
+                    break
+                if self.load_idx >= len(self.load_list):
+                    if self.fits_memory:
+                        self.running = False
+                        self.load_idx = 0
+                        break
+                    else:
+                        self.load_idx = 0
+                    self.update_cur_memory_size()
+                # Load one more file
+                d = dict()
+                sample_size = 0.0 
+                datapoints = 0
+                for dl in self.data_labels:
+                    d[dl], fsize = self.load_file(dl, self.load_list[self.load_idx])
+                    datapoints = d[dl].shape[0]
+                    sample_size += fsize
+                d['size'] = sample_size
+                d['datapoints'] = datapoints
+                self.memory.put(d)
+                self.load_idx += 1
+                self.update_cur_memory_size()
+            time.sleep(0.1)
+        if self.v:
+            if self.fits_memory:
+                print("Memory Loader ended as expected, everything fit into memory!")
+            else:
+                print("Memory Loader ended as expected!")
+
 class DataSet(data.Dataset):
     def __init__(self, dataset_params):
         super().__init__()
-        self.ds = dataset_params
-        self.data_info = []
+        self.dp = dataset_params
+        self.v = self.dp['verbose']
+
+        # Get basic info about the dataset from get_files
+        self.data_info = dict()
+        for dl in self.dp['data_labels']:
+            self.data_info[dl] = []
+        self.len = 0
+        self.dataset_size = 0.0
+        self.dataset_files = 0
         self.get_files()
-        self.get_total_data_size()
-        self.fill_cache()
+
+        self.one_file_size = 0.0
+        for dl in self.dp['data_labels']:
+            self.one_file_size += self.data_info[dl][0]['fsize']
 
         # Check if the full dataset fits in RAM
-        self.fit_ram = False
-        self.fit_vram = False
-        if self.get_total_data_size() < self.ds['ram']:
-            self.fit_ram = True
-        if self.get_total_data_size() < self.ds['vram']:
-            self.fit_vram = True
+        self.fit_memory = False
+        self.memory = Queue()
+        if self.dp['device'] == 'cpu':
+            assert self.one_file_size <= self.dp['memory'], \
+                    "Error, one file is bigger than Memory..."
+            if self.dataset_size < self.dp['memory']:
+                self.fit_memory = True
+            self.memory_loader = MemoryLoader(self.dp['memory'], self.memory, \
+                    self.one_file_size, self.data_info, self.fit_memory, \
+                    self.dp['data_labels'], self.dp['shuffle'], self.dp['device'], \
+                    self.v)
+        elif self.dp['device'] == 'cuda':
+            assert self.one_file_size <= self.dp['memory'], \
+                    "Error, one file is bigger than Memory..."
+            if self.dataset_size < self.dp['memory']:
+                self.fit_memory = True
+            self.memory_loader = MemoryLoader(self.dp['memory'], self.memory, \
+                    self.one_file_size, self.data_info, self.fit_memory, \
+                    self.dp['data_labels'], self.dp['shuffle'], self.dp['device'], \
+                    self.v)
+        else:
+            print("Error, unknown device: ", self.dp['device'])
+            exit()
 
-    def get_total_data_size(self):
-        return self.dataset_size
+        self.memory_loader.start()
 
-    def load_data(self, path, key, dtype):
-        with h5py.File(path) as f:
-            for dname, ds in f.items():
-                if dname == key:
-                    data = np.array(ds,dtype=dtype)
-                    self.cache[key] = data[:self.len]
-
-    def fill_cache(self):
-        self.cache = dict()
-        self.load_data(self.data_info[0]['data_path'], 'features', float)
-        self.load_data(self.data_info[0]['data_path'], 'labels', np.uint8)
+        self.cache = None
+        self.cache_idx = 0
 
     def get_files(self):
-        p = Path(self.ds['data_path'])
+        p = Path(self.dp['data_path'])
         assert p.is_dir()
         # Recursively find all h5 files
         files = sorted(p.glob('**/*.h5'))
         assert len(files) >= 1, 'No hdf5 datasets found in {}!'.format(p)
-        dataset_size = 0
         shapes = None
         for p in files:
             with h5py.File(p) as f:
-                for dname, ds in f.items():
-                    idx = -1
-                    # Make sure all datasets have the same size
-                    if shapes == None:
-                        shapes = np.array(ds).shape
-                    else:
-                        assert shapes[0] == np.array(ds).shape[0], "Error, some parts " \
-                                + "of the dataset have different amounts of datapoints"
-                    # Only take a part of the data when subset is specified
-                    assert self.ds['subset'] <= shapes[0] and self.ds['subset'] >= 0, \
-                            "Error, subset larger than amount of data"
-                    if self.ds['subset'] == 0:
-                        self.len = shapes[0]
-                    else:
-                        self.len = self.ds['subset']
+                for gname, group in f.items():
+                    for data_label, group2 in group.items():
+                        for data_name, ds in group2.items():
+                            # Make sure all datasets have the same size
+                            if shapes == None:
+                                shapes = np.array(ds).shape
+                            else:
+                                assert shapes[0] == np.array(ds).shape[0], "Error, some parts " \
+                                        + "of the dataset have different amounts of datapoints"
+                            # Only take a part of the data when subset is specified
+                            assert self.dp['subset'] <= shapes[0] and self.dp['subset'] >= 0, \
+                                    "Error, subset larger than amount of data"
 
-                    fsize = np.array(ds[:self.len]).nbytes/1024.0/1024.0
-                    dataset_size += fsize
-                    self.data_info.append({'data_path': p, 'data_label': dname, 'dtype': np.array(ds).dtype, \
-                            'shape': np.array(ds)[:self.len].shape, 'fsize': fsize})
-        self.dataset_size = dataset_size
+                            # Compute the amount of samples in the dataset
+                            if self.dp['subset'] == 0:
+                                if data_label == self.dp['data_labels'][0]:
+                                    self.len += shapes[0]
+                                    self.dataset_files += 1
+                            else:
+                                self.len = self.dp['subset']
+
+                            fsize = np.array(ds[:self.len]).nbytes/1024.0/1024.0
+                            self.dataset_size += fsize
+                            self.data_info[data_label].append({
+                                'data_path':p, 
+                                'data_label': data_label,
+                                'data_name': data_name,
+                                'dtype': np.array(ds).dtype,
+                                'shape': np.array(ds)[:self.len].shape, 
+                                'fsize': fsize,
+                                })
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, idx):
-        sample = dict()
-        for key in self.ds['data_labels']:
-            sample[key] = self.cache[key][idx]
-        return sample
+        if self.cache == None:
+            self.cache = self.memory.get()
+        elif idx-self.cache_idx >= self.cache['datapoints']:
+            # If all fit in memory the memory loader ends.
+            # So we have to put the taken file back into the queue 
+            # or we run out of data
+            if self.fit_memory:
+                self.memory.put(self.cache)
+            self.cache = self.memory.get()
+            self.cache_idx += self.cache['datapoints']
+        idx -= self.cache_idx
+        d = dict()
+        for dl in self.dp['data_labels']:
+            d[dl] = self.cache[dl][idx]
+        return d
+
--- tests/test_dataloader/test_dataset.py
+++ tests/test_dataloader/test_dataset.py
@@ -1,4 +1,5 @@
 import torch
+import time
 import pytest
 import psutil
 import os
@@ -7,36 +8,46 @@ import numpy as np
 import h5py
 from src.dataloader import DataLoader
 
-device = 'cpu'
-if torch.cuda.is_available():
-    device = 'cuda'
+# TODO test with only 1 feature and 3 features
+# Assert that the specified data_type in dataset_params is what comes from the hdf5
+# TODO what if dataset does not have the specified format?
 
 tmpdir = './tmp/'
 features_dir = tmpdir + 'features/'
 
-def get_dataset_features(dataset_size, sequence_size=2, feature_dims=[1], subset=0, \
-        vram=100, ram=100, differently_sized=False):
+def get_dataset_features(dataset_size, files=1, sequence_size=2, feature_dims=[1], \
+        subset=0, memory=100, differently_sized=False, device='cpu', shuffle=False):
     # Create tempdir
     shutil.rmtree(tmpdir, ignore_errors=True)
     os.mkdir(tmpdir)
     os.mkdir(features_dir)
     # Create some fake data
-    fake_data_features = np.ones(tuple([dataset_size, sequence_size] + feature_dims), dtype=float)
+    fake_data_features = np.zeros(tuple([dataset_size, sequence_size] + feature_dims), dtype=float)
     if differently_sized:
         fake_data_labels = np.ones((dataset_size-5, sequence_size, 1), dtype=np.uint8)
     else:
         fake_data_labels = np.ones((dataset_size, sequence_size, 1), dtype=np.uint8)
     with h5py.File(features_dir + 'data.h5', 'w') as f:
-        f.create_dataset('features', data=fake_data_features)
-        f.create_dataset('labels', data=fake_data_labels)
+        f.create_dataset('/data/features/0', data=fake_data_features)
+        f.create_dataset('/data/labels/0', data=fake_data_labels)
+    if files > 1:
+        for i in range(1,files,1):
+            with h5py.File(features_dir + 'data.h5', 'a') as f:
+                f.create_dataset(f'/data/features/{i}', data=fake_data_features)
+                f.create_dataset(f'/data/labels/{i}', data=fake_data_labels)
+    del fake_data_labels
+    del fake_data_features
 
-    dataset_params = {'ram' : ram,
-        'vram' : vram,
+    dataset_params = {'memory' : memory,
         'data_path' : features_dir,
         'data_labels' : ['features', 'labels'],
         'data_types' : [float, np.uint8],
+        'data_shapes' : [(tuple([sequence_size]+feature_dims)), (sequence_size, 1)],
         'batch_size' : 32,
         'subset' : subset,
+        'shuffle' : shuffle,
+        'device' : device,
+        'verbose' : False,
         }
 
     loader, dataset = DataLoader(dataset_params).get_loader()
@@ -52,6 +63,42 @@ def test_1D():
     for i, data in enumerate(loader, 0):
         assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
+
+def test_1D_multiple_files():
+    files = 2
+    dataset_size = 100
+    feature_dims = [1]
+    seq_size = 2
+    loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+            sequence_size=seq_size, feature_dims=feature_dims, files=2, subset=0)
+    assert dataset.__len__() == files*dataset_size
+    for i, data in enumerate(loader, 0):
+        assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
+            data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
+
+
+def test_1D_multiple_files_gpu():
+    files = 2
+    dataset_size = 100
+    feature_dims = [1]
+    seq_size = 2
+    device='cuda'
+    loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+            sequence_size=seq_size, feature_dims=feature_dims, files=2, subset=0, \
+            device=device)
+    assert dataset.__len__() == files*dataset_size
+    for i, data in enumerate(loader, 0):
+        assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
+            data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
 
 def test_1D_subset():
     subset = 10
@@ -67,6 +114,9 @@ def test_1D_subset():
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
         amount_of_data += data['features'].shape[0]
     assert amount_of_data == subset
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
 
 def test_1D_subset_too_large():
     subset = 101
@@ -78,6 +128,9 @@ def test_1D_subset_too_large():
                 sequence_size=seq_size, feature_dims=feature_dims, subset=subset)
         for i, data in enumerate(loader, 0):
             pass
+        dataset.memory_loader.stop()
+        time.sleep(0.2)
+        assert dataset.memory_loader.is_alive() == False
 
 def test_1D_different_size():
     subset = 10
@@ -90,6 +143,9 @@ def test_1D_different_size():
                 differently_sized=True)
         for i, data in enumerate(loader, 0):
             pass
+        dataset.memory_loader.stop()
+        time.sleep(0.2)
+        assert dataset.memory_loader.is_alive() == False
 
 def test_2D():
     dataset_size = 100
@@ -100,6 +156,9 @@ def test_2D():
     for i, data in enumerate(loader, 0):
         assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
 
 def test_3D():
     dataset_size = 100
@@ -110,33 +169,123 @@ def test_3D():
     for i, data in enumerate(loader, 0):
         assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
 
-def test_large_no_fit():
-    dataset_size = 1000
+def test_single_no_fit():
+    #In this test we test if one file is too large for the specified memory
+    dataset_size = 500
     feature_dims = [100]
     seq_size = 200
+    memory = 50.0
+    with pytest.raises(AssertionError, match='Error, one file is bigger than Memory...'):
+        loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+                sequence_size=seq_size, feature_dims=feature_dims, subset=0,\
+                memory=memory)
+        for i, data in enumerate(loader, 0):
+            pass
+        dataset.memory_loader.stop()
+        time.sleep(0.2)
+        assert dataset.memory_loader.is_alive() == False
+
+def test_multiple_no_fit():
+    # In this test we test if multiple small files that exceed memory crash
+    files=2
+    dataset_size = 500
+    feature_dims = [100]
+    seq_size = 200
+    memory = 100.0
     loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
-            sequence_size=seq_size, feature_dims=feature_dims, subset=0)
+            sequence_size=seq_size, feature_dims=feature_dims, subset=0,\
+            memory=memory, files=files)
     for i, data in enumerate(loader, 0):
         assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
-    assert dataset.fit_ram == False
-    assert dataset.fit_vram == False
+    assert dataset.fit_memory == False
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
 
 def test_large_ram_fit():
-    dataset_size = 1000
+    dataset_size = 100
     feature_dims = [100]
     seq_size = 200
-    vram = 100.0
-    ram = 1000.0
+    files=2
+    memory = 200.0
     loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
             sequence_size=seq_size, feature_dims=feature_dims, subset=0, \
-            vram=vram, ram=ram)
+            memory=memory, files=files)
     for i, data in enumerate(loader, 0):
         assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
             data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
-    assert dataset.fit_ram == True
-    assert dataset.fit_vram == False
-    # TODO large files and VRAm/RAM loading
-    # TODO test with only 1 feature and 3 features
-    # Assert that the specified data_type in dataset_params is what comes from the hdf5
+    assert dataset.fit_memory == True
+    dataset.memory_loader.stop()
+    time.sleep(0.2)
+    assert dataset.memory_loader.is_alive() == False
+
+def test_large_vram_fit():
+    # Test if 4 large files that do not all fit in VRAM work
+    dataset_size = 5000
+    feature_dims = [100]
+    seq_size = 200
+    files=4
+    memory = 2000.0
+    loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+            sequence_size=seq_size, feature_dims=feature_dims, subset=0, \
+            memory=memory, files=files, device='cpu')
+    data_amount = 0
+    for i, data in enumerate(loader, 0):
+        data_amount += data['features'].shape[0]
+    assert data_amount == files * dataset_size
+    assert dataset.fit_memory == False
+    dataset.memory_loader.stop()
+
+    while dataset.memory_loader.is_alive():
+        time.sleep(0.1)
+
+    assert dataset.memory_loader.is_alive() == False
+
+"""
+def test_large_vram_fit_shuffled():
+    dataset_size = 5000
+    feature_dims = [100]
+    seq_size = 200
+    files=4
+    memory = 2000.0
+
+    # True data
+    unshuffled_features = np.zeros(tuple([dataset_size, seq_size] + \
+            feature_dims), dtype=float)
+    unshuffled_labels = np.ones((dataset_size, seq_size, 1), dtype=np.uint8)
+
+    loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+            sequence_size=seq_size, feature_dims=feature_dims, subset=0, \
+            memory=memory, files=files, device='cpu', shuffle=True)
+    data_amount = 0
+    shuffled_features = np.zeros_like(unshuffled_features, dtype=float)
+    for i, data in enumerate(loader, 0):
+        pass
+
+    dataset.memory_loader.stop()
+    while dataset.memory_loader.is_alive():
+        time.sleep(0.1)
+    assert dataset.memory_loader.is_alive() == False
+
+def test_1D_multiple_files_shuffling():
+    # Test if multiple files are shuffled correctly
+    files = 2
+    dataset_size = 100
+    feature_dims = [1]
+    seq_size = 2
+    loader, dataset, dp = get_dataset_features(dataset_size=dataset_size, \
+            sequence_size=seq_size, feature_dims=feature_dims, files=2, subset=0)
+    assert dataset.__len__() == files*dataset_size
+    for i, data in enumerate(loader, 0):
+        assert data['features'].shape == (tuple([dp['batch_size'], seq_size] + feature_dims)) or \
+            data['features'].shape == (tuple([int(dataset.__len__()%dp['batch_size']), seq_size] + feature_dims))
+    dataset.memory_loader.stop()
+    while dataset.memory_loader.is_alive():
+        time.sleep(0.1)
+    assert dataset.memory_loader.is_alive() == False
+"""
