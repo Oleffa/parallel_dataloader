e0da26bcc09e50f8e37ce8588316912807da1f8b
Oliver <oliver.struckmeier@tutanota.de>
2022-06-02 13:43:29 +0300

    Made pytest requirement optional

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(name='parallel-dataloader',
         version='1.0',
+        packages=['parallel_dataloader'],
         install_requires=['torch==1.11', \
                 'h5py==3.7.0', \
                 'psutil==5.9.1', \
-                'numpy==1.22.4', \
-                'pytest==7.1.2'],
-        py_modules=['dataloader']
+                'numpy==1.22.4'],
+        extras_require={
+            'test' : ['pytest==7.1.2'],
+            },
 )
